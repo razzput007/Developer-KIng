6fd69d378b4ab4d321949fef7cd23c05e7485a9c
razzput007 <dhoom.rk74@gmail.com>
2022-11-01 14:23:40 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>DeveloperKing</h1>
+        <p>Copyright © 2022 right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://instagram.com/razzput007" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/meabhisingh" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
